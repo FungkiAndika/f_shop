881ce32373c0139862077881708a8628ec2e73ba
FungkiAndika <fungki4444@gmail.com>
2025-07-27 10:30:33 +0700

    .

--- src/utils/api/checkCountryAPI.js
+++ src/utils/api/checkCountryAPI.js
@@ -1,6 +1,6 @@
 async function checkCountry() {
     try {
-        return await fetch(process.env.REACT_APP_COUNTRY_CHECK_API_URL).then(res => res.json()).then(country => country.countryCode)
+        return await fetch(process.env.REACT_APP_COUNTRY_CHECK_API_URL).then(country => country.countryCode)
     } catch (error) {
         console.log("ERROR IN: checkCountry");
         
