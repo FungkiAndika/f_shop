be92112d07cf601a3dbd3466b51f17bd31f465bf
FungkiAndika <fungki4444@gmail.com>
2025-07-27 10:48:15 +0700

    .

--- src/utils/api/checkCountryAPI.js
+++ src/utils/api/checkCountryAPI.js
@@ -1,6 +1,9 @@
 async function checkCountry() {
     try {
         const res = await fetch(process.env.REACT_APP_COUNTRY_CHECK_API_URL)
+        if(!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
         const country = res.json()
         return country.country_code
     } catch (error) {
