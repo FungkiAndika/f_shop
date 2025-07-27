09ddf559a11442b5f647825d1a54d44426ad0c7d
FungkiAndika <fungki4444@gmail.com>
2025-07-27 10:23:27 +0700

    update f_shop/src/utils/api/checkCountryAPI.js

--- src/utils/api/checkCountryAPI.js
+++ src/utils/api/checkCountryAPI.js
@@ -1,6 +1,6 @@
 async function checkCountry() {
     try {
-        return await fetch(process.env.REACT_APP_COUNTRY_CHECK_API_URL).then(res => res.json()).then(country => country.country_code)
+        return await fetch(process.env.REACT_APP_COUNTRY_CHECK_API_URL).then(res => res.json()).then(country => country.countryCode)
     } catch (error) {
         console.log("ERROR IN: checkCountry");
         
